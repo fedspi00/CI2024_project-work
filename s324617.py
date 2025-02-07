b2e4883590f7841d67c24f1a1f66859a9bcaacf0
Federico Spinoso <s324617@studenti.polito.it>
2025-02-07 19:09:01 +0100

    Update s324617.py
    base functions for the formulas

--- s324617.py
+++ s324617.py
@@ -1,1 +1,28 @@
+import numpy as np
 
+def f0(x: np.ndarray) -> np.ndarray: 
+    return
+
+def f1(x: np.ndarray) -> np.ndarray: 
+    return
+  
+def f2(x: np.ndarray) -> np.ndarray: 
+    return
+
+def f3(x: np.ndarray) -> np.ndarray: 
+    return
+  
+def f4(x: np.ndarray) -> np.ndarray: 
+    return
+  
+def f5(x: np.ndarray) -> np.ndarray: 
+    return
+
+def f6(x: np.ndarray) -> np.ndarray: 
+    return
+
+def f7(x: np.ndarray) -> np.ndarray: 
+    return
+  
+def f8(x: np.ndarray) -> np.ndarray:
+    return
